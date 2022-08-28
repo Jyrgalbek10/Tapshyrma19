d383e11510ad593e36c1aa7766f53e03f5168c6e
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-08-28 15:47:43 +0300

    kon kono

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ZhumanynKundoru zhumanynKundoru = ZhumanynKundoru.valueOf(scanner.nextLine());
-        switch (zhumanynKundoru){
-            case DUISHOMBU :
-                System.out.println("Duishombu kunu java okuim");
-                break;
-            case SHEISHEMBI:
-                System.out.println("Anglis tili sabagyn okuim");
-                break;
-            case SHARSHEMBI:
-                System.out.println("Java sabagy");
-                break;
-            case BEISHEMBI:
-                System.out.println("Soft skills");
-                break;
-            case JUMA:
-                System.out.println("Java lesson");
-                break;
-            case ISHEMBI:
-                System.out.println("Oz aldyncha javany kaitaloo");
-                break;
-            case ZHEKSHEMBI:
-                System.out.println("Tapshyrma kyluu");
-                break;
-
+        ZhumanynKundoru[] zhumanynKundoru = {
+                ZhumanynKundoru.DUISHOMBU,
+                ZhumanynKundoru.SHEISHEMBI,
+                ZhumanynKundoru.SHARSHEMBI,
+                ZhumanynKundoru.BEISHEMBI,
+                ZhumanynKundoru.JUMA,
+                ZhumanynKundoru.ISHEMBI,
+                ZhumanynKundoru.ZHEKSHEMBI
+        };
+        for (ZhumanynKundoru z:zhumanynKundoru) {
+            System.out.println(z.getJumanynKundoru());
         }
     }
 }
--- src/ZhumanynKundoru.java
+++ src/ZhumanynKundoru.java
@@ -1,3 +1,25 @@
 public enum ZhumanynKundoru {
-    DUISHOMBU,SHEISHEMBI,SHARSHEMBI,BEISHEMBI,JUMA,ISHEMBI,ZHEKSHEMBI
+    DUISHOMBU("Dyishombu Java okuim"),SHEISHEMBI("Sheishembi kunu angliski"),
+    SHARSHEMBI("Sharshembi java"),BEISHEMBI("Beishembi soft skills "),JUMA("Juma Java"),
+    ISHEMBI("Ishembi java oz aldyncha"),ZHEKSHEMBI("Jekshembi tapshyrma");
+    private String jumanynKundoru;
+
+    ZhumanynKundoru(String jumanynKundoru) {
+        this.jumanynKundoru = jumanynKundoru;
+    }
+
+    public String getJumanynKundoru() {
+        return jumanynKundoru;
+    }
+
+    public void setJumanynKundoru(String jumanynKundoru) {
+        this.jumanynKundoru = jumanynKundoru;
+    }
+
+    @Override
+    public String toString() {
+        return "ZhumanynKundoru{ " +
+                "jumanynKundoru= '" + jumanynKundoru + '\'' +
+                '}';
+    }
 }
